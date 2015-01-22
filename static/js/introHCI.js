39df2798d26d8e4e75103f1c018fb29f8b3aaa03
Sean Rowan <sean.rowan15@gmail.com>
2015-01-22 13:35:21 -0800

    finished lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,51 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+
+    
+    var projectTitle = $(this).find("p").text();
+    console.log(projectTitle);
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+      }
+    }
+
+function changeStuff(e) {
+
+  console.log("In changeStuff");
+  var project = $("#project").val();
+  var width = $("#width").val();
+  console.log(project);
+  console.log(width);
+
+  var toChange = $(project).animate({width: $('#width').val()});
+
+  var newText = $('#description').val();
+  $(project + " .project-description").text(newText); 
+}
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$("#testjs").text("Tr0gs & K00ks");
+                $(".jumbotron p").toggleClass("active");
+		});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(changeStuff);
 }
